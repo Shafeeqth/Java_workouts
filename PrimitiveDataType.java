8ae0031674c8118cf7fe745fe789de45e0480eda
shafeeque <shafeeqsha06@gmail.com>
2024-11-19 17:04:11 +0530

    added workouts on primitives in java

---
+++ PrimitiveDataType.java
@@ -0,0 +1,30 @@
+public class PrimitiveDataType {
+   /* 
+    Primitive
+      - Int
+        - byte - 1 byte
+        - short - 2 byte
+        - int - 4 byte
+        - long - 8 byte
+
+      - Float
+        - double - 8 bytes - default 
+        - float - 4 bytes - eg-34.4f
+      - Char - 2 bytes
+      - Boolean - boolean
+   */  
+
+   int num = 1;
+   byte byte_num = 12;
+   short shrt = 222;
+   long lng = 2333343l;
+
+   float flt = 4.3f;
+   double dbl = 4.3;
+
+   char ch = 'c';
+
+   boolean bl = true;
+
+    
+} 
