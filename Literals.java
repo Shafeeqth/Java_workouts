ca7423854ff0b302a4d661d91cea0d91fbb639aa
shafeeque <shafeeqsha06@gmail.com>
2024-11-19 17:03:53 +0530

    added workouts on literals in java

---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Literals {
+    public static void main(String[] args) {
+        int num1 = 0b101;
+        int num2 = 0x5E;
+        System.out.println(num1);
+        System.out.println(num2);
+        long lg = 10_00_000;
+        char ch = 'c';
+        System.out.println(++ch);
+        System.out.println(lg );
+        System.out.println(num1);
+    }
+    
+}
