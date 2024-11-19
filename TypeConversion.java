dba601561ac7cfc5fe534f16811936e6918e3d58
shafeeque <shafeeqsha06@gmail.com>
2024-11-19 17:04:32 +0530

    added workouts on typeconversions in java

---
+++ TypeConversion.java
@@ -0,0 +1,12 @@
+import java.lang.ProcessBuilder.Redirect.Type;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+       int num1 = 1;
+       byte byt1 = 23;
+       num1 = byt1;
+       System.out.println();
+
+
+    }
+}
