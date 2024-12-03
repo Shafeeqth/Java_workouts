4c985245d0c3b84e0fa59aee3b7125c075769848
shafeeque <shafeeqsha06@gmail.com>
2024-12-03 23:11:46 +0530

    added workouts on string in java

---
+++ Stringg/StringBuff.java
@@ -0,0 +1,15 @@
+package Stringg;
+
+public class StringBuff {
+    public static void main(String[] args) {
+        StringBuffer strBuff = new StringBuffer("My name is Shafeeque");
+        
+        System.out.println(strBuff);
+        strBuff.insert(0, "Hello ");
+        System.out.println(strBuff);
+
+
+
+    }
+    
+}
---
+++ Stringg/StringDemo.java
@@ -0,0 +1,24 @@
+package Stringg;
+
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String name = new String("Naji");
+
+        
+        String str = "Shafeeque";
+        System.out.println(str);
+        
+        String str1 = "Shafeeque";
+        System.out.println(str == str1);
+
+        str = "Muhammed " + str;
+        System.out.println(str);
+
+        System.out.println();
+        // String Buffer
+
+        
+    }
+    
+}
