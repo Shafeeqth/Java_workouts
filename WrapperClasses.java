82fe0dda82a30c18270bc377249b4421be9dd470
shafeeque <shafeeqsha06@gmail.com>
2024-12-14 22:57:39 +0530

    added workouts on wrapper classes

---
+++ WrapperClasses.java
@@ -0,0 +1,14 @@
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+        Integer num1 = new Integer(8);
+        int num = 7;
+        Integer num2 = num;
+        int num3 = num2;
+        System.out.println();
+
+        System.out.println(num1.intValue());
+
+    }
+
+}
